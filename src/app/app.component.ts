8dd088e45b2ff38a96f080ac04492d8c986d4655
SThimma2 <Srinivas.thimmanapalli2@t-mobile.com>
2020-08-30 11:38:13 -0400

    Date Picker changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularDatePicker';
+  roomsFilter: any = {};
+
+  @ViewChild('myDatepicker', {static: false}) datePicker;
+
+  printDate() {
+    console.log('date picker --> ' + this.datePicker._selected);
+  }
+
+  public onChange(event: any): void {
+    console.log(event);
+    // this.getData(newDate);
+  }
 }
